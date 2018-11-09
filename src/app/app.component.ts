223cbcaaa9885edb89fb31424e1c5d1decc5c982
Aaron Chavez <achavez@corporaciondpt.com>
2018-11-08 18:22:20 -0600

    prime / bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  val: number;
+
+  title = 'The FUNNY but SAD website';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+
+import {SliderModule} from 'primeng/slider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SliderModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
